41b4724ff3c9a60b9373b7dc8495ddf4fa7edb36
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-08-17 09:19:45 -0600

    Adding a notifier in login form provider

--- lib/features/auth/presentation/providers/login_form_provider.dart
+++ lib/features/auth/presentation/providers/login_form_provider.dart
@@ -14,6 +14,55 @@ final loginFormProvider =
   );
 });
 
+//! 2 - Como implementar un notifier
+class LoginFormNotifier extends StateNotifier<LoginFormState> {
+  final Function(String, String) loginUserCallback;
+
+  LoginFormNotifier({
+    required this.loginUserCallback,
+  }) : super(LoginFormState());
+
+  onEmailChange(String value) {
+    final newEmail = Email.dirty(value);
+    state = state.copyWith(
+      email: newEmail,
+      isValid: Formz.validate([newEmail, state.password]),
+    );
+  }
+
+  onPasswordChanged(String value) {
+    final newPassword = Password.dirty(value);
+    state = state.copyWith(
+      password: newPassword,
+      isValid: Formz.validate([newPassword, state.email]),
+    );
+  }
+
+  onFormSubmit() async {
+    _touchEveryField();
+
+    if (!state.isValid) return;
+
+    state = state.copyWith(isPosting: true);
+
+    await loginUserCallback(state.email.value, state.password.value);
+
+    state = state.copyWith(isPosting: false);
+  }
+
+  _touchEveryField() {
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+
+    state = state.copyWith(
+      isFormPosted: true,
+      email: email,
+      password: password,
+      isValid: Formz.validate([email, password]),
+    );
+  }
+}
+
 //! 1- State del provider
 
 class LoginFormState {
@@ -56,50 +105,5 @@ class LoginFormState {
         email: $email,
         password: $password,
     ''';
-  }
-}
-
-//! 2 - Como implementar un notifier
-class LoginFormNotifier extends StateNotifier<LoginFormState> {
-  final Function(String, String) loginUserCallback;
-
-  LoginFormNotifier({
-    required this.loginUserCallback,
-  }) : super(LoginFormState());
-
-  onEmailChange(String value) {
-    final newEmail = Email.dirty(value);
-    state = state.copyWith(
-      email: newEmail,
-      isValid: Formz.validate([newEmail, state.password]),
-    );
-  }
-
-  onPasswordChanged(String value) {
-    final newPassword = Password.dirty(value);
-    state = state.copyWith(
-      password: newPassword,
-      isValid: Formz.validate([newPassword, state.email]),
-    );
-  }
-
-  onFormSubmit() async {
-    _touchEveryField();
-
-    if (!state.isValid) return;
-
-    await loginUserCallback(state.email.value, state.password.value);
-  }
-
-  _touchEveryField() {
-    final email = Email.dirty(state.email.value);
-    final password = Password.dirty(state.password.value);
-
-    state = state.copyWith(
-      isFormPosted: true,
-      email: email,
-      password: password,
-      isValid: Formz.validate([email, password]),
-    );
   }
 }
