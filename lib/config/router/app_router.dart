c2b820e56426859cc5b6aa96e3825918efc11ed7
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-08-17 09:24:14 -0600

    Add validations to navigate based on authentication status

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,26 +1,63 @@
 import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
+import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/login',
-  routes: [
-    ///* Auth Routes
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
+import 'app_router_notifier.dart';
 
-    ///* Product Routes
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ],
+final goRouterProvider = Provider(
+  (ref) {
+    final goRouterNotifier = ref.read(goRouterNotifierProvider);
 
-  ///! TODO: Bloquear si no se está autenticado de alguna manera
+    return GoRouter(
+      initialLocation: '/splash',
+      refreshListenable: goRouterNotifier,
+      routes: [
+        ///* Primera ruta
+        GoRoute(
+          path: '/splash',
+          builder: (context, state) => const CheckAuthStatusScreen(),
+        ),
+
+        ///* Auth Routes
+        GoRoute(
+          path: '/login',
+          builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          path: '/register',
+          builder: (context, state) => const RegisterScreen(),
+        ),
+
+        ///* Product Routes
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const ProductsScreen(),
+        ),
+      ],
+      redirect: (context, state) {
+        final isGoingTo = state.matchedLocation;
+        final authStatus = goRouterNotifier.authStatus;
+
+        if (isGoingTo == '/splash' && authStatus == AuthStatus.checking)
+          return null;
+
+        if (authStatus == AuthStatus.unAuthenticated) {
+          if (isGoingTo == '/login' || isGoingTo == '/register') return null;
+          return '/login';
+        }
+
+        if (authStatus == AuthStatus.authenticated) {
+          if (isGoingTo == '/login' ||
+              isGoingTo == '/register' ||
+              isGoingTo == '/splash') {
+            return '/';
+          }
+        }
+
+        return null;
+      },
+    );
+  },
 );
